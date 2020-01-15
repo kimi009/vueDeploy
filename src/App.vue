76e8549ad1fafdd6395016ce1fc955a04d2dd280
chenfuye <444812002@qq.com>
2020-01-15 08:50:49 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="vue  docker部署测试999" />
+    <HelloWorld msg="vue  docker部署999" />
   </div>
 </template>
 
